4080b56529f4a90a4bc6ac18b40e17fe01bb9efe
AberDerBart <nonatz@web.de>
2017-02-04 23:24:18 +0100

    improved interface

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ kivy.require('1.0.6')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.clock import Clock
+from kivy.properties import ObjectProperty
 
 import Interface
 
@@ -28,8 +30,17 @@ class ButtonBox(Widget):
 	pass
 
 class MpdApp(App):
+	artist=ObjectProperty(None)
+	title=ObjectProperty(None)
+
 	def build(self):
 		return PlayerWidget()
-
+	def update(self,dt):
+		current=Interface.currentsong()
+		if(current):
+			self.artist.text=current["artist"]
+			self.title.text=current["title"]
 if __name__ == '__main__':
-	MpdApp().run()
+	app=MpdApp()
+	Clock.schedule_interval(app.update, .1)
+	app.run()
