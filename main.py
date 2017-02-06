97c0598c4188668b70dae597fc35edbfacb17ab0
AberDerBart <nonatz@web.de>
2017-02-07 00:56:54 +0100

    added settings screen with connection settings (the settings are not used yet)

--- main.py
+++ main.py
@@ -2,50 +2,9 @@ import kivy
 kivy.require('1.0.6')
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import Screen, ScreenManager, NoTransition
-from kivy.clock import Clock
-from kivy.properties import ObjectProperty
+from kivy.uix.screenmanager import ScreenManager, NoTransition
 
-import Interface
-
-from Settings import MpcSettings
-
-class PlayerWidget(Screen):
-	artist=ObjectProperty(None)
-	title=ObjectProperty(None)
-	progress=ObjectProperty(None)
-	drawer=ObjectProperty(None)
-	def on_pre_enter(self):
-		self.clock=Clock.schedule_interval(self.update,.1)
-	def on_leave(self):
-		self.clock.cancel()
-		self.drawer.state="closed"
-	def update(self,dt):
-		if(Interface.update() or self.title.text==""):
-			"""if there is a current song, display it, else display nothing"""
-			current=Interface.currentsong()
-			if(current):
-				self.artist.text=current["artist"]
-				self.title.text=current["title"]
-			else:
-				self.artist.text=""
-				self.title.text=""
-
-			"""if the song updates, update the progressbar"""
-			status=Interface.status()	
-			if(status and "duration" in status and "elapsed" in status):
-				self.progress.max=float(status["duration"])
-				self.progress.value=float(status["elapsed"])
-			else:
-				self.progress.value=0
-		else:
-			"""if playing, let the progress bar run"""
-			if(Interface.state=="play"):
-				self.progress.value+=dt
-
-class PlaybackButtons(Widget):
-	pass
+from Player import PlayerWidget
 
 class MpdApp(App):
 
