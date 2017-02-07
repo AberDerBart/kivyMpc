edc5510acd4655ef2ec8c68d3eeab60e39664239
AberDerBart <nonatz@web.de>
2017-02-07 11:09:06 +0100

    implemented reading of config

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, NoTransition
 
 from Player import PlayerWidget
+from Interface import iFace
 
 class MpdApp(App):
 
 	def build(self):
+		iFace.host=self.config.get("Connection","host")
+		iFace.port=self.config.getint("Connection","port")
+		print("host:"+iFace.host)
 		sm=ScreenManager()
 		sm.transition=NoTransition()
 		sm.add_widget(PlayerWidget(name="player"))
@@ -17,6 +21,12 @@ class MpdApp(App):
 		config.setdefaults("Connection",{"host":"localhost","port":6600})
 	def build_settings(self,settings):
 		settings.add_json_panel("Connection", self.config, "connectionSettings.json")
+	def on_config_change(self, config, section, key, value):
+		if section=="Connection":
+			if key=="host":
+				iFace.host=value
+			if key=="port":
+				iFace.port=value
 
 if __name__ == '__main__':
 	app=MpdApp()
