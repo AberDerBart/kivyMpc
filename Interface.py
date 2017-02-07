0740040c7247ae60e172daeb83adc628456ac04c
AberDerBart <nonatz@web.de>
2017-02-07 12:15:24 +0100

    improved background functionality

--- Interface.py
+++ Interface.py
@@ -17,10 +17,22 @@ class Interface(EventDispatcher):
 	
 	def __init__(self):
 		self.client=MPDClient()
-		self.thread=None
-		self.connected=True
-		self.status=None
-		self.propLock=threading.Lock()
+
+		self._thread=None
+		self._task=None
+		self._task_reply=None
+	def _exec_task(self,task):
+		if not self._task_done():
+			return False
+		self._task=task
+		self._thread=threading.Thread(target=self._task,args=())
+		self._thread.start()
+		return True
+	def _task_done(self):
+		if not self._thread:
+			return True
+		return not self._thread.is_alive()
+
 	def _end_idle(self):
 		canRead=select([self.client],[],[],0)[0]
 
@@ -31,23 +43,27 @@ class Interface(EventDispatcher):
 			return None
 	def connect(self):
 		if(self.connectionState=="disconnected"):
-			if (not self.thread) or (not self.thread.is_alive()):
-				self.thread=threading.Thread(target=self._connect,args=())
+			if self._task_done():
 				self.connectionState="connecting"
-				self.thread.start()
+				self._exec_task(self._connect)
 		elif(self.connectionState=="connecting"):
-			if(self.thread and not self.thread.is_alive()):
-				self.connectionState="connected"
+			if(self._task_done()):
+				if(self._task_reply):
+					self.connectionState="connected"
+				else:
+					self.connectionState="disconnected"
 	def _connect(self):
+		self._task_reply=False
 		try:
 			self.client.connect(self.host,self.port)
-			self.connected=True
+			self._task_reply=True
 			self._fetch()
 			self.client.send_idle()
 		except MPDError as e:
 			print("MPDError: "+str(e))
 		except Exception as e:
 			print("Exception: "+str(e))
+
 
 	def disconnect(self):
 		try:
