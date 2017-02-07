1c31ade536ee9167da41695f7d4d4afbf064c942
AberDerBart <nonatz@web.de>
2017-02-07 03:02:05 +0100

    implemented EventDispatcher-based Interface (to be improved)

--- Interface.py
+++ Interface.py
@@ -1,103 +1,109 @@
 from mpd import MPDClient, MPDError, ConnectionError
 from select import select
+from kivy.event import EventDispatcher
+from kivy.properties import StringProperty,NumericProperty
 
-client=MPDClient()
-connected=False
-state=None
+class Interface(EventDispatcher):
+	artist=StringProperty("")
+	title=StringProperty("")
+	elapsed=NumericProperty(0)
+	duration=NumericProperty(1)
+	state=StringProperty("")
+	
+	
+	def __init__(self):
+		self.client=MPDClient()
+		self.connected=False
+		self.status=None
+		self.host="localhost"
+		self.port=6600
+	def _end_idle(self):
+		canRead=select([self.client],[],[],0)[0]
 
-def end_idle():
-	canRead=select([client],[],[],0)[0]
+		if(canRead):
+			return self.client.fetch_idle()
+		else:
+			self.client.noidle()
+			return None
+	def connect(self):
+		if not self.connected:
+			try:
+				self.client.connect(self.host,self.port)
+				self.connected=True
+				self._fetch()
+				self.client.send_idle()
+			except MPDError as e:
+				print("MPDError: "+str(e))
+			except Exception as e:
+				print("Exception: "+str(e))
 
-	if(canRead):
-		return client.fetch_idle()
-	else:
-		client.noidle()
-		return None
-def connect():
-	global connected
-	if not connected:
+	def disconnect(self):
 		try:
-			client.connect("localhost",6600)
-			client.send_idle()
-			connected=True
+			self.connected=False
+			self.client.disconnect()
 		except MPDError as e:
 			print("MPDError: "+str(e))
 		except Exception as e:
 			print("Exception: "+str(e))
+	def _fetch(self):
+		print("fetch")
+		status=self.client.status()
+		if(status):
+			print("status")
+			self.duration=float(status.get("duration","1"))
+			self.elapsed=float(status.get("elapsed","0"))
+			self.state=status.get("state","")
 
-def disconnect():
-	global connected
-	try:
-		connected=False
-		client.disconnect()
-	except MPDError as e:
-		print("MPDError: "+str(e))
-	except Exception as e:
-		print("Exception: "+str(e))
-
-def update():
-	connect()
-
-	try:
-		ret=end_idle()
-		client.send_idle()
-		return ret
-	except ConnectionError as e:
-		disconnect()
-	except MPDError as e:
-		print("MPDError: "+str(e))
-	except Exception as e:
-		print("Exception: "+str(e))
-		disconnect()
-	return None
-
-def _command(command):
-	ret=None
-	try:
-		end_idle()
+		current=self.client.currentsong()
+		if current:
+			print("current")
+			self.artist=current.get("artist","")
+			self.title=current.get("title","")
+	def update(self):
+		self.connect()
 		try:
-			ret=command(client)
+			ret=self._end_idle()
+
+			if ret:
+				self._fetch()
+			self.client.send_idle()
+		except ConnectionError as e:
+			self.disconnect()
 		except MPDError as e:
-			print("Command failed: "+str(e))
-		client.send_idle()
-	except ConnectionError as e:
-		disconnect()
-	except MPDError as e:
-		print("MPDError: "+str(e))
-	except Exception as e:
-		print("Exception: "+str(e))
-		disconnect()
-	return ret
+			print("MPDError: "+str(e))
+		except Exception as e:
+			print("Exception: "+str(e))
+			self.disconnect()
+		return None
+	def _command(self,command):
+		ret=None
+		try:
+			self._end_idle()
+			try:
+				ret=command(self.client)
+			except MPDError as e:
+				print("Command failed: "+str(e))
+			self.client.send_idle()
+		except ConnectionError as e:
+			self.disconnect()
+		except MPDError as e:
+			print("MPDError: "+str(e))
+		except Exception as e:
+			print("Exception: "+str(e))
+			self.disconnect()
+		return ret
+	def play(self):
+		return self._command(MPDClient.play)
 
-def currentsong():
-	return _command(MPDClient.currentsong)
+	def toggle(self):
+		if(self.state):
+			if(self.state=="play"):
+				self._command(MPDClient.pause)
+			else:
+				self._command(MPDClient.play)
+	def next(self):
+		return self._command(MPDClient.next)
+	def prev(self):
+		return self._command(MPDClient.previous)
 
-def playlist():
-	return _command(MPDClient.playlistinfo)
-
-def play():
-	return _command(MPDClient.play)
-
-def toggle():
-	if(state and state in ["play"]):
-		_command(MPDClient.pause)
-	else:
-		_command(MPDClient.play)
-
-def status():
-	global state
-	s=_command(MPDClient.status)
-	if s and "state" in s:
-		state=s["state"]
-	return s
-
-def next():
-	return _command(MPDClient.next)
-
-def prev():
-	return _command(MPDClient.previous)
-
-def close():
-	end_idle()
-	client.close()
-
+iFace=Interface()
--- Player.py
+++ Player.py
@@ -9,9 +9,6 @@ class PlaybackButtons(Widget):
 	pass
 
 class PlayerWidget(Screen):
-	artist=ObjectProperty(None)
-	title=ObjectProperty(None)
-	progress=ObjectProperty(None)
 	drawer=ObjectProperty(None)
 	def on_pre_enter(self):
 		self.clock=Clock.schedule_interval(self.update,.1)
@@ -19,24 +16,6 @@ class PlayerWidget(Screen):
 		self.clock.cancel()
 		self.drawer.state="closed"
 	def update(self,dt):
-		if(Interface.update() or self.title.text==""):
-			"""if there is a current song, display it, else display nothing"""
-			current=Interface.currentsong()
-			if(current):
-				self.artist.text=current["artist"]
-				self.title.text=current["title"]
-			else:
-				self.artist.text=""
-				self.title.text=""
-
-			"""if the song updates, update the progressbar"""
-			status=Interface.status()	
-			if(status and "duration" in status and "elapsed" in status):
-				self.progress.max=float(status["duration"])
-				self.progress.value=float(status["elapsed"])
-			else:
-				self.progress.value=0
-		else:
-			"""if playing, let the progress bar run"""
-			if(Interface.state=="play"):
-				self.progress.value+=dt
+		Interface.iFace.update()
+		pass
+		#this is to be adapted for the new mpc interface
