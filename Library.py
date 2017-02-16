34c519abe1faf9056695ca78213811f871eeb6a9
AberDerBart <nonatz@web.de>
2017-02-16 16:02:04 +0100

    added preliminary LibraryItem implementation

--- Library.py
+++ Library.py
@@ -1,4 +1,23 @@
 from kivy.uix.screenmanager import Screen
+from Interface import iFace
+from kivy.properties import StringProperty
+from kivy.uix.listview import SelectableView
+from kivy.uix.boxlayout import BoxLayout
+
+class LibraryItem(SelectableView,BoxLayout):
+	infoDict={}
+	dataType=StringProperty()
+	def __init__(self, **kwargs):
+		self.infoDict = kwargs.pop('text', {})
+		self.dataType=kwargs.pop('type',"")
+		super(PlaylistItem, self).__init__(**kwargs)
+
+	def displayText(self):
+		return "test"
+	def addAction(self):
+		print("add")
+	def openAction(self):
+		print("open")
 
 class LibraryWidget(Screen):
 	pass
