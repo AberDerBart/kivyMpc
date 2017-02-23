abee23efb217a5e42c2ba1ef32f2a08b293043dd
AberDerBart <nonatz@web.de>
2017-02-23 18:43:56 +0100

    added volume button and dropdown list

--- NavBar.py
+++ NavBar.py
@@ -1,5 +1,16 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.dropdown import DropDown
+from kivy.uix.button import Button
 
 class NavBar(BoxLayout):
 	screen=ObjectProperty()
+
+class VolumeDropDown(DropDown):
+	pass
+
+class VolumeButton(Button):
+	dropDown=ObjectProperty()
+	def __init__(self, **kwargs):
+		self.dropDown=VolumeDropDown()
+		super(VolumeButton,self).__init__(**kwargs)
