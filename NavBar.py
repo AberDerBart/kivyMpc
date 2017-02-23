ffe39f7dfc3b42027e311d97ad80fc62f382d456
AberDerBart <nonatz@web.de>
2017-02-23 18:58:23 +0100

    added classes MenuButton and MenuIconButton

--- NavBar.py
+++ NavBar.py
@@ -2,6 +2,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.dropdown import DropDown
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+
+class MenuButton(Button):
+	pass
+
+class MenuIconButton(MenuButton):
+	pass
+
+class MenuLabel(Label):
+	pass
 
 class NavBar(BoxLayout):
 	screen=ObjectProperty()
@@ -9,7 +19,7 @@ class NavBar(BoxLayout):
 class VolumeDropDown(DropDown):
 	pass
 
-class VolumeButton(Button):
+class VolumeButton(MenuButton):
 	dropDown=ObjectProperty()
 	def __init__(self, **kwargs):
 		self.dropDown=VolumeDropDown()
