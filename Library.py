7c173af6b0a0f29b7cfb5e4b6508e66e7c4a8d02
AberDerBart <nonatz@web.de>
2017-03-31 00:37:00 +0200

    implemented grey background for songs in playlist in library view

--- Library.py
+++ Library.py
@@ -1,13 +1,16 @@
 from kivy.uix.screenmanager import Screen
 from Interface import iFace
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, ObjectProperty
 
 class LibraryItem(BoxLayout):
 	artist=StringProperty()
 	title=StringProperty()
 	file=StringProperty()
+	background=ObjectProperty((0,0,0))
 	def __init__(self, **kwargs):
+		iFace.bind(playlist=self.updatePlaylist)
+		self.bind(file=self.updatePlaylist)
 		super(LibraryItem, self).__init__(**kwargs)
 
 	def displayText(self,artist,title,fileName):
@@ -17,6 +20,17 @@ class LibraryItem(BoxLayout):
 			return fileName
 	def addAction(self):
 		iFace.add(self.file)
+	def updatePlaylist(self,instance,value):
+		contained=False
+		for item in iFace.playlist:
+			if item.get("file")==self.file:
+				contained=True
+		
+		if(contained):
+			self.background=(.1,.1,.1)
+		else:
+			self.background=(0,0,0)
+
 
 class LibraryWidget(Screen):
 	pass
